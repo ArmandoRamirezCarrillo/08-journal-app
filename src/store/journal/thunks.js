8cfc5e77395728a004879cff7d974be47ed5b9c9
Jose Armando Ramirez Carrillo <ing.armandoramirez@outlook.com>
2024-06-10 17:50:04 -0600

    Creando el store para las notas

---
+++ src/store/journal/thunks.js
@@ -0,0 +1,21 @@
+import { collection, doc, setDoc } from 'firebase/firestore/lite';
+import { firebaseDB } from '../../firebase/config';
+import { addNewEmptyNote, savingNewNote, setActiveNote } from './';
+
+export const startNewNote = () => {
+    return async(dispatch, getState) => {
+        dispatch(savingNewNote());
+        const {uid} = getState().auth;
+        const newNote = {
+            title: '',
+            body: '',
+            date: new Date().getTime(),
+        };
+        collection(firebaseDB, 'users');
+        const newDoc = doc(collection(firebaseDB, `${uid}/journal/notes`));
+        await setDoc(newDoc,newNote);
+        newNote.id = newDoc.id;
+        dispatch(addNewEmptyNote(newNote));
+        dispatch(setActiveNote(newNote));
+    }
+}
